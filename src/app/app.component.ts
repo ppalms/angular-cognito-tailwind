88a9f2c68c55989431350874fb470c09d44b1f31
Patrick Palmer <ppalmer@pm.me>
2022-02-06 12:22:28 -0600

    Add Tailwind CSS with basic app shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'boilerplate';
+  title = 'Boilerplate';
+  viewTitle = this.title;
+
+  // TODO
+  loggedIn = true;
+  showUserMenu = false;
+
+  constructor() {}
+
+  toggleUserMenu() {
+    this.showUserMenu = !this.showUserMenu;
+  }
+
+  logIn(event: MouseEvent) {
+    event.preventDefault();
+
+    console.warn('Implement logIn()');
+  }
+
+  logOut(event: MouseEvent) {
+    event.preventDefault();
+
+    console.warn('Implement logOut()');
+  }
+
+  // Changes the view title when a new route is activated in router-outlet
+  onActivate(event: any) {
+    if (event?.title && typeof event.title === 'string') {
+      this.viewTitle = event.title;
+    } else {
+      this.viewTitle = 'Boilerplate';
+    }
+  }
+
+  // Hides the user dropdown menu if the user clicks anywhere else on the page
+  @HostListener('document:click', ['$event'])
+  onClick = (e: any) => {
+    const clickedElement = e.target as HTMLElement;
+    if (
+      !clickedElement ||
+      !clickedElement.parentElement ||
+      clickedElement.parentElement.getAttribute('id') !== 'user-menu-button'
+    ) {
+      this.showUserMenu = false;
+    }
+  };
 }
